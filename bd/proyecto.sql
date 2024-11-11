e4b2efad0c7aef4050d16d2ac020ca42c287e67f
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplsencia.esm>
2024-11-11 21:17:59 +0100

    Cambio imagenes y BD

--- bd/proyecto.sql
+++ bd/proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2024 a las 19:16:41
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 11-11-2024 a las 21:16:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `asociados` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `logo` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `descripcion` text COLLATE utf8_spanish_ci DEFAULT NULL
+  `nombre` varchar(255) NOT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -42,7 +42,7 @@ CREATE TABLE `asociados` (
 
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(255) NOT NULL,
   `numImagenes` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -51,9 +51,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
-(1, 'Categoría 1', 1),
-(2, 'Categoría 2', 0),
-(3, 'Categoría 3', 0);
+(1, 'Categoría 1', 4),
+(2, 'Categoría 2', 4),
+(3, 'Categoría 3', 4);
 
 -- --------------------------------------------------------
 
@@ -63,8 +63,8 @@ INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
 
 CREATE TABLE `imagenes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `descripcion` text COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text DEFAULT NULL,
   `numVisualizaciones` int(11) DEFAULT 0,
   `numLikes` int(11) DEFAULT 0,
   `numDownloads` int(11) DEFAULT 0,
@@ -76,7 +76,18 @@ CREATE TABLE `imagenes` (
 --
 
 INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `numLikes`, `numDownloads`, `categoria`) VALUES
-(9, 'GatoInformatico.jpg', 'Gato Informático', 0, 0, 0, 1);
+(1, '1.jpg', 'Img 1', 7871, 1923, 4308, 1),
+(2, '2.jpg', 'Img 2', 1296, 3347, 197, 1),
+(3, '3.jpg', 'Img 3', 4803, 651, 778, 1),
+(4, '4.jpg', 'Img 4', 2529, 846, 3911, 1),
+(5, '5.jpg', 'Img 5', 5470, 5750, 7603, 2),
+(6, '6.jpg', 'Img 6', 7178, 7760, 2285, 2),
+(7, '7.jpg', 'Img 7', 8452, 6710, 187, 2),
+(8, '8.jpg', 'Img 8', 9269, 5972, 8011, 2),
+(9, '9.jpg', 'Img 9', 6839, 3305, 477, 3),
+(10, '10.jpg', 'Img 10', 4201, 3814, 6620, 3),
+(11, '11.jpg', 'Img 11', 2859, 6930, 8342, 3),
+(12, '12.jpg', 'Img 12', 7217, 2239, 9099, 3);
 
 -- --------------------------------------------------------
 
@@ -86,11 +97,11 @@ INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `nu
 
 CREATE TABLE `mensajes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `apellidos` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `asunto` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `texto` text COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `asunto` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `texto` text DEFAULT NULL,
   `fecha` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -143,7 +154,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes`
