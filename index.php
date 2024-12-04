bee2562ab75a8a64a88debb9dc12aa7261ebfda9
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplsencia.esm>
2024-12-04 16:25:13 +0100

    Cambios

--- index.php
+++ index.php
@@ -5,4 +5,25 @@
     $uri = trim($_SERVER['REQUEST_URI'], '/');
 
     require $routers[$uri];
+
+/*
+function esOpcionMenuActiva(string $opcionMenu): bool {
+        $active = false;
+    
+        // OBTENGO LA URL ACTUAL
+        $uri = $_SERVER['REQUEST_URI'];
+    
+        // CONDICIÓN ESPECIAL PARA HOME
+        if ($opcionMenu === 'index' && ($uri === '/' || $uri === '/index.php')) {
+            return true;
+        }
+    
+        // VERIFICO QUE LA OPCIÓN DEL MENÚ ESTÁ EN LA URL
+        if (strpos($uri, $opcionMenu) !== false) {
+            return true;
+        }
+    
+        return $active;
+    }
+*/
 ?>
