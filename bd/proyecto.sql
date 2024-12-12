39caa4307d6a7800f28518037bf68b37a56a0480
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplasencia.es>
2024-12-12 19:35:12 +0100

    Funciona

--- bd/proyecto.sql
+++ bd/proyecto.sql
@@ -41,8 +41,7 @@ CREATE TABLE `asociados` (
 INSERT INTO `asociados` (`id`, `nombre`, `logo`, `descripcion`) VALUES
 (1, 'Logo 1', 'log1.jpg', 'Logo 1'),
 (2, 'Logo 2', 'log2.jpg', 'Logo 2'),
-(3, 'Logo 3', 'log3.jpg', 'Logo 3'),
-(4, 'Logo 4', 'log4.jpg', 'Logo 4');
+(3, 'Logo 3', 'log3.jpg', 'Logo 3');
 
 -- --------------------------------------------------------
 
@@ -61,9 +60,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `numImagenes`) VALUES
-(1, 'Categoría 1', 4),
-(2, 'Categoría 2', 4),
-(3, 'Categoría 3', 4);
+(1, 'Categoría 1', 0),
+(2, 'Categoría 2', 0),
+(3, 'Categoría 3', 0);
 
 -- --------------------------------------------------------
 
@@ -80,24 +79,6 @@ CREATE TABLE `imagenes` (
   `numDownloads` int(11) DEFAULT 0,
   `categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `imagenes`
---
-
-INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `numVisualizaciones`, `numLikes`, `numDownloads`, `categoria`) VALUES
-(1, '1.jpg', 'Img 1', 7871, 1923, 4308, 1),
-(2, '2.jpg', 'Img 2', 1296, 3347, 197, 1),
-(3, '3.jpg', 'Img 3', 4803, 651, 778, 1),
-(4, '4.jpg', 'Img 4', 2529, 846, 3911, 1),
-(5, '5.jpg', 'Img 5', 5470, 5750, 7603, 2),
-(6, '6.jpg', 'Img 6', 7178, 7760, 2285, 2),
-(7, '7.jpg', 'Img 7', 8452, 6710, 187, 2),
-(8, '8.jpg', 'Img 8', 9269, 5972, 8011, 2),
-(9, '9.jpg', 'Img 9', 6839, 3305, 477, 3),
-(10, '10.jpg', 'Img 10', 4201, 3814, 6620, 3),
-(11, '11.jpg', 'Img 11', 2859, 6930, 8342, 3),
-(12, '12.jpg', 'Img 12', 7217, 2239, 9099, 3);
 
 -- --------------------------------------------------------
 
